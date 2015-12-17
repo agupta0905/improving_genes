68acbd4fb94efdc86554d3f67dca31362f18cc67
Ashu Gupta <agupta@Ashus-MacBook-Pro.local>
2015-12-17 16:26:54 -0600

    Updated source

--- tmp/11-taxon/split_true_genetrees.py
+++ tmp/11-taxon/split_true_genetrees.py
@@ -1,5 +1,8 @@
-truegenesfile='/u/sciteam/gupta1/scratch/11_taxon_dataset/model.10.5400000.0.000000037/1/truegenetrees'
-output_dir='/u/sciteam/gupta1/scratch/11_taxon_dataset/model.10.5400000.0.000000037/1/true_gene_trees'
+m1='model.10.5400000.0.000000037'
+m3='model.10.600000.0.000000333'
+presentmodel=m3
+truegenesfile='/u/sciteam/gupta1/scratch/11_taxon_dataset/'+presentmodel+'/1/truegenetrees'
+output_dir='/u/sciteam/gupta1/scratch/11_taxon_dataset/'+presentmodel+'/1/true_gene_trees'
 f=open(truegenesfile,'r')
 for linenumber,line in enumerate(f):
     gene = linenumber+1
