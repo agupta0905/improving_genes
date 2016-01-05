75582e2f2dbee675c35690fa11595383f9ca5379
Ashu Gupta <agupta@Ashus-MacBook-Pro.local>
2016-01-05 07:20:01 +0530

    Taking care of empty lines

--- tmp/astral2/sample_data.py
+++ tmp/astral2/sample_data.py
@@ -11,16 +11,18 @@ def sample_data(filepath,outfilepath,num_taxa,num_sites,num_genes):
     f_out=open(outfilepath,'w')
     curr_gene=0
     for line in f:
-        words=line.split(' ',1)
-        currtaxa=int(words[0])
-        if int(currtaxa==total_taxa):
-            curr_gene+=1
-            if curr_gene>num_genes:
-                break
-            f_out.write(str(num_taxa)+' '+str(num_sites)+'\n')
-        elif currtaxa<num_taxa:
-            words[1]=words[1].strip()
-            f_out.write(str(currtaxa)+' '+words[1][:num_sites]+'\n')
+        line=line.strip()
+        if line:
+            words=line.split(' ',1)
+            currtaxa=int(words[0])
+            if int(currtaxa==total_taxa):
+                curr_gene+=1
+                if curr_gene>num_genes:
+                    break
+                f_out.write(str(num_taxa)+' '+str(num_sites)+'\n')
+            elif currtaxa<num_taxa:
+                words[1]=words[1].strip()
+                f_out.write(str(currtaxa)+' '+words[1][:num_sites]+'\n')
     f.close()
     f_out.close()
                 
